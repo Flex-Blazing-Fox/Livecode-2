0ec2a62e767483a1d2c0b6cea4ba720dbb831346
ardiansyakh <abdullah.ard9@gmail.com>
2021-08-10 19:09:44 +0700

    1

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+'use strict';
+const bcrypt = require('bcrypt')
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    hooks: {
+      boforeCreate: (User)=>{
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(User.password, salt)
+        User.password = hash
+      }
+    },
+    modelName: 'User',
+  });
+  return User;
+};
---
+++ server/router/index.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const {UserController, AnimalsController, FavoriteController} = require('./controller')
+
+module.exports = router
