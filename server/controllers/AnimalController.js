bdbb4898eb7c1e9a98ad7a1ae2d6d239d4923af8
Arip Budiman <aripbudiman007@gmail.com>
2021-08-10 19:08:32 +0700

    commit 1

---
+++ server/controllers/AnimalController.js
@@ -0,0 +1,5 @@
+class AnimalController {
+
+}
+
+module.exports = AnimalController
---
+++ server/controllers/UserController.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+class UserController {
+    static register(req, res, next) {
+
+    }
+
+    static login(req, res, next){
+
+    }
+}
+
+module.exports = UserController
